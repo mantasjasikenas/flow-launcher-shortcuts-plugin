fe14048f38e5f4f212d2f4e497e6cde4f5d6b9c6
Mantelis <mantasjasikenas@gmail.com>
2024-10-03 12:19:22 +0300

    Fix navigation view and title bar issues

--- Flow.Launcher.Plugin.ShortcutPlugin.App/Views/ShellPage.xaml.cs
+++ Flow.Launcher.Plugin.ShortcutPlugin.App/Views/ShellPage.xaml.cs
@@ -5,13 +5,12 @@ using Flow.Launcher.Plugin.ShortcutPlugin.App.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 
 using Windows.System;
 
 namespace Flow.Launcher.Plugin.ShortcutPlugin.App.Views;
 
-// TODO: Update NavigationViewItem titles and icons in ShellPage.xaml.
+
 public sealed partial class ShellPage : Page
 {
     public ShellViewModel ViewModel
@@ -27,12 +26,36 @@ public sealed partial class ShellPage : Page
         ViewModel.NavigationService.Frame = NavigationFrame;
         ViewModel.NavigationViewService.Initialize(NavigationViewControl);
 
-        // A custom title bar is required for full window theme and Mica support.
-        // https://docs.microsoft.com/windows/apps/develop/title-bar?tabs=winui3#full-customization
         App.MainWindow.ExtendsContentIntoTitleBar = true;
+        App.MainWindow.SetTitleBarBackgroundColors(Microsoft.UI.Colors.Transparent);
         App.MainWindow.SetTitleBar(AppTitleBar);
+
         App.MainWindow.Activated += MainWindow_Activated;
         AppTitleBarText.Text = "AppDisplayName".GetLocalized();
+
+        SetupNavigationMenu();
+    }
+
+    private void SetupNavigationMenu()
+    {
+        var home = new NavigationViewItem
+        {
+            Content = "Shell_Main".GetLocalized(),
+            Icon = new SymbolIcon(Symbol.Home),
+            Tag = typeof(MainPage)
+        };
+        var shortcuts = new NavigationViewItem
+        {
+            Content = "Shell_Shortcuts".GetLocalized(),
+            Icon = new SymbolIcon(Symbol.AllApps),
+            Tag = typeof(ShortcutsPage),
+        };
+
+        NavigationHelper.SetNavigateTo(home, typeof(MainViewModel).FullName);
+        NavigationHelper.SetNavigateTo(shortcuts, typeof(ShortcutsViewModel).FullName);
+
+        NavigationViewControl.MenuItems.Add(home);
+        NavigationViewControl.MenuItems.Add(shortcuts);
     }
 
     private void OnLoaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
@@ -45,18 +68,7 @@ public sealed partial class ShellPage : Page
 
     private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
     {
-        App.AppTitlebar = AppTitleBarText as UIElement;
-    }
-
-    private void NavigationViewControl_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
-    {
-        AppTitleBar.Margin = new Thickness()
-        {
-            Left = sender.CompactPaneLength * (sender.DisplayMode == NavigationViewDisplayMode.Minimal ? 2 : 1),
-            Top = AppTitleBar.Margin.Top,
-            Right = AppTitleBar.Margin.Right,
-            Bottom = AppTitleBar.Margin.Bottom
-        };
+        App.AppTitlebar = AppTitleBar as UIElement;
     }
 
     private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null)
@@ -80,5 +92,26 @@ public sealed partial class ShellPage : Page
         var result = navigationService.GoBack();
 
         args.Handled = result;
+    }
+
+    private void NavigationView_DisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
+    {
+        if (args.DisplayMode == NavigationViewDisplayMode.Compact || args.DisplayMode == NavigationViewDisplayMode.Minimal)
+        {
+            PaneToggleBtn.Visibility = Visibility.Visible;
+            AppTitleBar.Margin = new Thickness(48, 0, 0, 0);
+            AppTitleBarText.Margin = new Thickness(12, 0, 0, 0);
+        }
+        else
+        {
+            PaneToggleBtn.Visibility = Visibility.Collapsed;
+            AppTitleBar.Margin = new Thickness(16, 0, 0, 0);
+            AppTitleBarText.Margin = new Thickness(16, 0, 0, 0);
+        }
+    }
+
+    private void PaneToggleBtn_Click(object sender, RoutedEventArgs e)
+    {
+        NavigationViewControl.IsPaneOpen = !NavigationViewControl.IsPaneOpen;
     }
 }
