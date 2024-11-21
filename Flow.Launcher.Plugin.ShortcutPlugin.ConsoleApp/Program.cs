3684209922bd3e2a5fb3bd2c00cdf036e641c8aa
Mantelis <mantasjasikenas@gmail.com>
2024-11-21 12:50:37 +0200

    Add Shortcuts JSON schema generation

---
+++ Flow.Launcher.Plugin.ShortcutPlugin.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using Flow.Launcher.Plugin.ShortcutPlugin.ConsoleApp;
+
+
+var schema = JsonSchema.GenerateShortcutSchema();
+
+var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+var path = Path.Combine(desktop, "shortcuts-schema.json");
+
+File.WriteAllText(path, schema);
